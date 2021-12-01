4c76e83038e0610b4a5699caf7276a8be328eada
Rob C <hyakuhei@gmail.com>
2021-12-01 15:38:40 -0800

    Basic DSL parsing to JSON completed and working

--- ltm.py
+++ ltm.py
@@ -1,92 +1,141 @@
-
 grammar = """
-    start: scene | dataflow
+    start: command+
 
-    scene: "scene:" [WS] ESCAPED_STRING                       -> scene
+    command: dataflow   
+           | scene      
+           | boundary 
 
-    dataflow: _actor WS _actor ":" [WS] flow                 -> dataflow
-    _actor: WORD                                             
-    flow: [ESCAPED_STRING | protocol]                      
-    protocol: WORD "(" [ESCAPED_STRING | protocol] ")"
+    boundary: "boundary" SPACE ESCAPED_STRING ":" WORD+
 
-    %import common.WS
+    dataflow: pitcher SPACE catcher ":"[SPACE][ flow | protocol ]
+    pitcher: WORD
+    catcher: WORD
+    flow: ESCAPED_STRING
+    protocol: WORD "(" [ protocol | flow ]")"
+
+    scene: "scene:"[SPACE]ESCAPED_STRING
+
+    SPACE: " "
+
+    STRING: ESCAPED_STRING
+
     %import common.WORD
     %import common.ESCAPED_STRING
+    %import common.WS
 
+    %ignore WS
 """
 
-from lark import Token
+import json
+import logging
+
+from deepdiff import DeepDiff
+
 from lark import Lark
-from lark.tree import Tree
+from lark.visitors import Visitor_Recursive
+
 parser = Lark(grammar)
 
-currentScene = None
+doc = {"actors": {}, "boundaries": {}, "scenes": []}
 
-scenes = {'currentScene':"default"}
+globalContext = {"currentScene": "UNSET"}
 
-def _addScene(title):    
-    scenes['currentScene'] = title
-    scenes[scenes['currentScene']] = []
-    
-    print(scenes)
+_sceneMap = {}
 
-def run_instruction(t):
-    if t.data == 'scene':
-        for c in t.children:
-            if c is not None:
-                if c.type == "ESCAPED_STRING":
-                    _addScene(str(c))
-       
-    if t.data == 'dataflow':
-        # actor actor: <Protocol>("message") 
-        # Protocol is optional
-        # Protocol can be nested
+class MyVisitor(Visitor_Recursive):
+    # This has to be run top-down.
+    def _addActor(self, tree):
+        if globalContext["currentScene"] == "UNSET":
+            logging.error(
+                'All Dataflows must belong to a scene. Hint: define a scene using: scene:"TheScene"'
+            )
 
-        left = None #will be string
-        right = None #will be string
-        flow = None #will be string or Protocol()
-        for c in t.children:
-            if isinstance(c, Token):
-                if c.type == "WORD":
-                    if left is None:
-                        left = str(c)
-                    else:
-                        right = str(c)
+        if str(tree.children[0]) not in doc["actors"]:
+            doc["actors"][str(tree.children[0])] = {}
 
-            if isinstance(c, Tree):
-                print(c.children)
-                for tc in c.children:
-                    if isinstance(tc, Token):
-                        if tc.type == "ESCAPED_STRING":
-                            flow = str(tc)
-                        else:
-                            print(f"Found some other type {flow}")
+    def pitcher(self, tree):
+        self._addActor(tree)
+
+    def catcher(self, tree):
+        self._addActor(tree)
+
+    def scene(self, tree):
+        sceneName = str(tree.children[1]).strip('"\\')
+        globalContext["currentScene"] = sceneName
+        if sceneName not in _sceneMap:
+            scene = {sceneName:[]}
+            doc["scenes"].append(scene)
+            _sceneMap[sceneName] = scene[sceneName] # Look up the right scene in the [] of scenes
+
+    def dataflow(self, tree):
+        flow = {}
+        protocols = []
+        parentProtocol = flow  # Flow is kind of always an anonymous protocol
+
+        for sub in tree.iter_subtrees():
+            if sub.data == "pitcher":
+                flow["from"] = str(sub.children[0])
+            if sub.data == "catcher":
+                flow["to"] = str(sub.children[0])
+            if sub.data == "protocol":
+                # Nested protocol
+                print(f"Found protocol: {str(sub.children[0])}")
+                protocols.append(str(sub.children[0]))
+            if sub.data == "flow":
+                # Check to see if we have protocols that this should be nestled in
+                parentProtocol["data"] = str(sub.children[0]).strip('"\\')
+
+        protocols.reverse()
+        for p in protocols:
+            newProtocol = {"name": p}
+            parentProtocol["protocol"] = newProtocol
+            parentProtocol = newProtocol
+
+        _sceneMap[globalContext["currentScene"]].append(flow)
+        #doc["scenes"][globalContext["currentScene"]].append(flow)
+
+    def boundary(self, tree):
+        thisBoundary = None
+        for child in tree.children[1:]:
+            if child.type == "ESCAPED_STRING":
+                thisBoundary = str(child).strip('"\\')
+                if thisBoundary not in doc["boundaries"]:
+                    doc["boundaries"][thisBoundary] = []
+            elif child.type == "WORD":
+                assert thisBoundary in doc["boundaries"]
+                doc["boundaries"][thisBoundary].append(str(child))
 
 
-        print(f"left: {left}, right: {right}, flow: {flow}")
+## TODO: Add any additional enrichment magic (like say, required fields for a protocol)
+def enrich(doc: dict):
+    pass
 
-def run_ltm(program):
-    parse_tree = parser.parse(program)
-    print(parser.parse(program).pretty())
-    for inst in parse_tree.children:
-        run_instruction(inst)
-
-def main():
-    while True:
-        code = input('> ')
-        try:
-            run_ltm(code)
-        except Exception as e:
-            print(e)
+#diagram as code
+def dac(doc, engine: str = "dot"):
+    pass
 
 def test():
-    textSimple = '''
-        scene:"jeff"
-        user service:TCP(TLS(HTTP("GET /")))
-    '''
+    with open("testInput.ltm", "r") as sample:
+        # Parse the input file
+        parseTree = parser.parse(sample.read())
+        MyVisitor().visit_topdown(parseTree)
+        print(json.dumps(doc, indent=4, sort_keys=True))
 
-    print(parser.parse(textSimple).pretty())
+        # Compare test input with hand crafted json
+        with open("targetTestOutput.json", "r") as testTarget:
+            testDoc = json.loads(testTarget.read())
+            diff = DeepDiff(testDoc, doc)
+            if diff == {}:
+                print("PASSED TEST")
+            else:
+                print(diff)
+        
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
-    #main()
+    # main()
+    # commands:
+    ## input-file
+    ## input-format
+    ## output-file
+    ## output-file
